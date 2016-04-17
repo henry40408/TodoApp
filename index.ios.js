38ec890868e85516f94fed0f16e17ad36552f940
Heng-Yi Wu <henry40408@gmail.com>
2016-04-17 18:37:55 +0800

    add mock data

--- index.ios.js
+++ index.ios.js
@@ -3,6 +3,12 @@ import React, {
   View,
 } from 'react-native';
 
+const todoItems = [
+  'buy some books',
+  'cut the rope',
+  'remember the milk',
+];
+
 class TodoApp extends React.Component {
   render() {
     return (<View />);
